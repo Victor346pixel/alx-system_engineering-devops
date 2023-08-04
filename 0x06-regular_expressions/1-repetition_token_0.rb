ae5014a3aac1bd6346c65f93762a6f5a7726e6fa
Victor346pixel <monicamwaniki84@gmail.com>
2023-08-04 11:31:46 +0300

    updated

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+#
+#def validate_string(input_string)
+#  pattern = /^[A-Z]+[a-z]+-[0-9]+$/
+#    if input_string.match?(pattern)
+#        puts "Valid input: #{input_string}"
+#          else
+#              puts "Invalid input: #{input_string}"
+#                end
+#                end
+#
+#                # Check if the script is run with an argument
+#                if ARGV.length != 1
+#                  puts "Usage: ruby script_name.rb <input_string>"
+#                  else
+#                    input_string = ARGV[0]
+#                      validate_string(input_string)
+#                      end
+#
