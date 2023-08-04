abc4aa8e917538c79a64c4684f109ca29ffbe992
Victor346pixel <monicamwaniki84@gmail.com>
2023-08-04 11:22:46 +0300

    updated

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#
+## Define the regular expression to match the word "School"
+#regex_pattern = /School/
+#
+## Accept one argument from the command line
+#input_argument = ARGV[0]
+#
+## Function to match the argument against the regular expression
+#def match_regex(input, regex)
+#  if input =~ regex
+#      puts "Match found: #{input}"
+#        else
+#            puts "No match found."
+#              end
+#              end
+#
+#              # Call the function with the input argument and regex pattern
+#              match_regex(input_argument, regex_pattern)
+#
